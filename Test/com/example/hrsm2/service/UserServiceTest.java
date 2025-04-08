37a178a0efacd250d23e8b9048f058ec85a5f737
Michael Nagi <michael.nagui.kiriakos@gmail.com>
2025-04-08 17:21:16 +0200

    newest

--- Test/com/example/hrsm2/service/UserServiceTest.java
+++ Test/com/example/hrsm2/service/UserServiceTest.java
@@ -12,12 +12,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserServiceTest {
 
     private UserService operation;
-    private User admin;
 
     @BeforeEach
     void setUp() {
         operation = UserService.getInstance();
-        admin = new User("miky2004", "miky123", "Michael Nagi", User.UserRole.HR_ADMIN);
     }
 
     @AfterEach
@@ -30,7 +28,13 @@ class UserServiceTest {
 
     @Test
     void testAuthenticate() {
-        assertSame(admin, operation.authenticate("miky2004", "miky123"));
+        User expectedUser1 = new User("miky2004", "miky123", "Michael Nagi", User.UserRole.HR_ADMIN);
+        assertEquals(expectedUser1.toString(), operation.authenticate("miky2004", "miky123").toString());
+
+        User expectedUser2 = new User("super", "super123", "Super Administrator", User.UserRole.HR_ADMIN);
+        assertEquals(expectedUser2.toString(),operation.authenticate("super", "super123").toString());
+
+        assertNotEquals(expectedUser1.toString(),operation.authenticate("super", "super123").toString());
     }
 
     @Test
@@ -63,8 +67,8 @@ class UserServiceTest {
 
     @Test
     void testIsUsernameTaken() {
-        assertTrue(operation.isUsernameTaken("super"));         // super exists already
+        assertTrue(operation.isUsernameTaken("super"));         // super already exists
         assertFalse(operation.isUsernameTaken("fadij1234"));    // fadij1234 doesn't exists
-        assertTrue(operation.isUsernameTaken("miky2004"));      // miky2004 doesn't exist
+        assertTrue(operation.isUsernameTaken("miky2004"));      // miky2004 already exist
     }
 }
