bc6f6ebe03f146754122501235e3337ddb9a4c34
Fady John <fadijohn9@gmail.com>
2025-04-10 18:26:11 +0200

    EmployeeService, UserService, PerformanceEvaluationService, half of PayrollService Testing is done. 3.5/5 classes are completed testing.

---
+++ Test/com/example/hrsm2/service/PayrollServiceTest.java
@@ -0,0 +1,58 @@
+package com.example.hrsm2.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+// michael hal el file da 3andak ???
+class PayrollServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void getAllPayrolls() {
+    }
+
+    @Test
+    void getPayrollById() {
+    }
+
+    @Test
+    void getPayrollsByEmployeeId() {
+    }
+
+    @Test
+    void generatePayroll() {
+    }
+
+    @Test
+    void processPayroll() {
+    }
+
+    @Test
+    void markPayrollAsPaid() {
+    }
+
+    @Test
+    void updatePayroll() {
+    }
+
+    @Test
+    void deletePayroll() {
+    }
+
+    @Test
+    void generatePayrollsForAllEmployees() {
+    }
+}
